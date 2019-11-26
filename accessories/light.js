d44e17327254bbaca1011bbc287bfa973b142b28
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2019-11-27 09:42:25 +1300

    Update light.js

--- accessories/light.js
+++ accessories/light.js
@@ -114,7 +114,7 @@ class LightAccessory extends SwitchAccessory {
       const foundValues = this.dataKeys('hue');
       const closest = foundValues.reduce((prev, curr) => Math.abs(curr - state.hue) < Math.abs(prev - state.hue) ? curr : prev);
       const hexData = data[`hue${closest}`];
-      
+
       log(`${name} setHue: (closest: hue${closest})`);
       await this.performSend(hexData);
     });
