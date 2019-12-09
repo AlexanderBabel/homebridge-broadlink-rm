d4ebd15d732e2f43b7305918d793f2b3903cc8a4
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2019-12-10 11:41:06 +1300

    Added retry when the device is offline

--- helpers/sendData.js
+++ helpers/sendData.js
@@ -3,15 +3,18 @@ const assert = require('assert')
 const { getDevice } = require('./getDevice');
 const convertProntoCode = require('./convertProntoCode')
 
-module.exports = ({ host, hexData, log, name, debug }) => {
+const retryInterval = 3000;
+const retryCount = 6;
+
+const sendData = async ({host, hexData, log, name, debug}) => {
   assert(hexData && typeof hexData === 'string', `\x1b[31m[ERROR]: \x1b[0m${name} sendData (HEX value is missing)`);
 
   // Check for pronto code
   if (hexData.substring(0, 4) === '0000') {
     if (debug) log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Converting Pronto code "${hexData}" to Broadlink code)`);
-    hexData = convertProntoCode(hexData)
+    hexData = convertProntoCode(hexData);
     if (debug) log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Pronto code successfuly converted: "${hexData}")`);
-    
+
     if (!hexData) return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (A Pronto code was detected however its conversion to a Broadlink code failed.)`);
 
   }
@@ -28,8 +31,29 @@ module.exports = ({ host, hexData, log, name, debug }) => {
   if (!device.sendData) return log(`\x1b[31m[ERROR] \x1b[0mThe device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);
   if (hexData.includes('5aa5aa555')) return log(`\x1b[31m[ERROR] \x1b[0mThis type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.`);
 
+  await waitForDevice(device, name, log, debug);
+
   const hexDataBuffer = new Buffer(hexData, 'hex');
   device.sendData(hexDataBuffer, debug, hexData);
 
   log(`${name} sendHex (${device.host.address}; ${device.host.macAddress}) ${hexData}`);
 }
+
+const waitForDevice = async (device, name, log, debug) => {
+  let count = 0;
+
+  while (device.state !== 'active' && count < retryCount ) {
+    log(`\x1b[33m[DEBUG]\x1b[0m ${name} Device ${device.state}, pausing (attempt ${count})...`);
+    count++;
+
+    await wait(retryInterval);
+  }
+
+  if (count > retryCount) return log(`\x1b[31m[ERROR] \x1b[0mThe device at ${device.host.address} (${device.host.macAddress}) is not available.`);
+
+  return;
+}
+
+const wait = delay => { return new Promise (res => { setTimeout(res, delay); }); }
+
+module.exports = sendData
