82f26fa60bc6c9ab2a433476df1faae329d1e712
Cameron McConnochie <32912464+kiwi-cam@users.noreply.github.com>
2020-01-09 11:17:08 +1300

    Fixed error in new state logic

--- helpers/getDevice.js
+++ helpers/getDevice.js
@@ -38,8 +38,10 @@ const startPing = (device, log) => {
     try {
       ping.pingHost(device.host.address, (err, target) => {
         if(err){
-           log(`\x1b[31m[ERROR] \x1b[0m Error pinging Broadlink RM device. (${target}, ${error.message})`);
-           active = false;
+          log(`\x1b[31m[ERROR] \x1b[0m Error pinging Broadlink RM device. (${target}, ${error.message})`);
+          active = false;
+        }else{
+          active = true;
         }
         
         if(active){ 
